155845ea61309e28aa17cd28313804d8d125646c
ccckmit <ccckmit@gmail.com>
2019-06-22 17:48:23 +0800

    add md needed js, css

--- doc/main.js
+++ doc/main.js
@@ -3,12 +3,16 @@ var sidebar = null
 function init() {
   sidebar = document.querySelector('aside')
   sidebar.style.width = '0px'
-  document.addEventListener("DOMContentLoaded", function () {
-    var mathElements = document.getElementsByClassName("math");
-    for (var i = 0; i < mathElements.length; i++) {
-      var texText = mathElements[i].firstChild;
-      if (mathElements[i].tagName == "SPAN") { katex.render(texText.data, mathElements[i], { displayMode: mathElements[i].classList.contains("display"), throwOnError: false } );
-  }}});
+  var mathElements = document.getElementsByClassName("math");
+  for (var i = 0; i < mathElements.length; i++) {
+    var texText = mathElements[i].firstChild;
+    if (mathElements[i].tagName == "SPAN") {
+      katex.render(texText.data, mathElements[i], { 
+        displayMode: mathElements[i].classList.contains("display"), 
+        throwOnError: false
+      })
+    }
+  }
 }
 
 function toggleSidebar() {
